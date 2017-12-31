6b808c761430384063365bf1560fe3d7272a7fc0
Jonathan <jhony-913@hotmail.com>
2017-12-30 19:11:27 -0500

    Vector 3D

---
+++ 2º Parcial/Deberes/WinAppVectores3D/WinAppVectores2D/frmVectors3D.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppVectores2D
+{
+    public partial class frmVectors2D : Form
+    {
+
+        //Datos Miembro (Composicion Simple)
+
+        private CVector3D A = new CVector3D();
+        private CVector3D B = new CVector3D();
+        private CVector3D R = new CVector3D();
+
+        public frmVectors2D()
+        {
+            InitializeComponent();
+            R.GraphAxis(picCanvas);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdition_Click(object sender, EventArgs e)
+        {
+            A.ReadData(nudXVectorA, nudYVectorA, nudZVectorA);
+            B.ReadData(nudXVectorB, nudYVectorB, nudZVectorB);
+
+            R = R.Addition(A, B);
+            //R = A.Addition(B);
+            //R = B.Addition(A);
+
+            R.PrintData(txtXVectorR,txtYVectorR, txtZVectorR);
+            R.GraphAxis(picCanvas);
+            A.Graficadora(A, picCanvas, Color.Red);
+            B.Graficadora(B, picCanvas, Color.Blue);
+            R.Graficadora(R, picCanvas, Color.Green);
+
+        }
+
+        private void btnSustraction_Click(object sender, EventArgs e)
+        {
+            A.ReadData(nudXVectorA, nudYVectorA, nudZVectorA);
+            B.ReadData(nudXVectorB, nudYVectorB, nudZVectorB);
+
+            R = R.Sustraction(A, B);
+            // R = A.Sustraction(B);
+         
+            R.PrintData(txtXVectorR, txtYVectorR, txtZVectorR);
+            R.GraphAxis(picCanvas);
+            A.Graficadora(A, picCanvas, Color.Red);
+            B.MY = B.MY * (-1);
+            B.MX = B.MX * (-1);
+            B.Graficadora(B, picCanvas, Color.Blue);
+            R.Graficadora(R, picCanvas, Color.Green);
+
+        }
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            A.ReadData(nudXVectorA, nudYVectorA, nudZVectorA);
+            B.ReadData(nudXVectorB, nudYVectorB, nudZVectorB);
+
+            R = R.ProdcutoPunto(A, B);
+            //R = A.ProdcutoPunto(B);
+
+            R.PrintData(txtXVectorR, txtYVectorR, txtZVectorR);
+            R.GraphAxis(picCanvas);
+            A.Graficadora(A, picCanvas, Color.Red);
+            B.MY = B.MY * (-1);
+            B.MX = B.MX * (-1);
+            B.Graficadora(B, picCanvas, Color.Blue);
+            R.Graficadora(R, picCanvas, Color.Green);
+
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            A.ReadData(nudXVectorA, nudYVectorA, nudZVectorA);
+            B.ReadData(nudXVectorB, nudYVectorB, nudZVectorB);
+
+            R = R.ProductoCruz(A, B);
+            //R = A.ProductoCruz(B);
+
+            R.PrintData(txtXVectorR, txtYVectorR, txtZVectorR);
+            R.GraphAxis(picCanvas);
+            A.Graficadora(A, picCanvas, Color.Red);
+            B.MY = B.MY * (-1);
+            B.MX = B.MX * (-1);
+            B.Graficadora(B, picCanvas, Color.Blue);
+            R.Graficadora(R, picCanvas, Color.Green);
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmVectors2D_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGraf_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblImaginario_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
